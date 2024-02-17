d39abcf987d48da7367a46a8b5afa7cb4c29d680
Manisa <manisarajemisa24@gmail.com>
2024-02-18 01:10:54 +0300

    fix: endpoint names

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express= require('express')
-const cors=require('cors');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const managerRouter = require('./routes/manager/ManagerRoute.js')
-app.use('/managers',managerRouter)
+const managerRouter = require("./routes/manager/ManagerRoute.js");
+app.use("/api/managers", managerRouter);
 
-const ServiceRouter = require('./routes/service/ServiceRoute.js')
-app.use('/services',ServiceRouter)
+const ServiceRouter = require("./routes/service/ServiceRoute.js");
+app.use("/api/services", ServiceRouter);
 
-const EmployeRouter = require('./routes/employe/EmployeRoute.js')
-app.use('/employes',EmployeRouter)
+const EmployeRouter = require("./routes/employe/EmployeRoute.js");
+app.use("/api/employes", EmployeRouter);
 
-const CustomerRouter = require('./routes/customer/CustomerRoute.js')
-app.use('/customers',CustomerRouter)
+const CustomerRouter = require("./routes/customer/CustomerRoute.js");
+app.use("/api/customers", CustomerRouter);
 
-const rendezvousRouter = require('./routes/rendezvous/RendezvousRoute.js')
-app.use('/rendezvous',rendezvousRouter)
+const rendezvousRouter = require("./routes/rendezvous/RendezvousRoute.js");
+app.use("/api/rendezvous", rendezvousRouter);
 
-app.listen(5000, () => {console.log("http://localhost:5000/")})
-
+app.listen(5000, () => {
+  console.log("http://localhost:5000/");
+});
