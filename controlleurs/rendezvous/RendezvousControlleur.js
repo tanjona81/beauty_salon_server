b2647d749343560bc3be1fdc3a54482c7d745ffe
Manisa <manisarajemisa24@gmail.com>
2024-02-18 01:14:56 +0300

    feat: improve error handling and format the response

--- controlleurs/rendezvous/RendezvousControlleur.js
+++ controlleurs/rendezvous/RendezvousControlleur.js
@@ -1,127 +1,289 @@
-const uri = require('../../config/DbConfig.js')
-const mongoose = require('mongoose');
-const service = require('../../services/rendezvous/RendezvousServices.js')
+const uri = require("../../config/DbConfig.js");
+const mongoose = require("mongoose");
+const service = require("../../services/rendezvous/RendezvousServices.js");
+const HttpStatus = require("http-status-codes");
 
 const getRendezvous = () => {
-    return(async (req,res)=>{
-        try{
-            // await mongoose.connect(uri)
-            await service.getAll()
-            .then((result)=>{
-                if(result.length<=0) return res.status(204).send('No match for the request')
-                return res.status(200).json(result)
-                
-            })
-            .catch((err) => {
-                console.log("Error : "+err.message)
-                return res.status(500).send('Internal server error')
-            });
-        }catch(e){
-            console.log("Error : "+e.message)
-            // await mongoose.disconnect()
-            return res.status(500).send('Internal server error')
-        }finally{
-            // await mongoose.disconnect()
-        }
-    })
-}
+  return async (req, res) => {
+    try {
+      // await mongoose.connect(uri)
+      await service
+        .getAll()
+        .then((result) => {
+          const responseData = {
+            status: true,
+            message: "List rendezvous",
+            details: result,
+            http_response: {
+              message: HttpStatus.getStatusText(HttpStatus.OK),
+              code: HttpStatus.OK,
+            },
+          };
+          return res.status(HttpStatus.OK).json(responseData);
+        })
+        .catch((err) => {
+          const responseData = {
+            status: false,
+            message: err,
+            details: null,
+            http_response: {
+              message: HttpStatus.getStatusText(
+                HttpStatus.INTERNAL_SERVER_ERROR
+              ),
+              code: HttpStatus.INTERNAL_SERVER_ERROR,
+            },
+          };
+          return res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(responseData);
+        });
+    } catch (e) {
+      const responseData = {
+        status: false,
+        message: e,
+        details: null,
+        http_response: {
+          message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR),
+          code: HttpStatus.INTERNAL_SERVER_ERROR,
+        },
+      };
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(responseData);
+    } finally {
+      // await mongoose.disconnect()
+    }
+  };
+};
 
 const getRendezvousById = () => {
-    return(async (req,res)=>{
-        try{
-            // await mongoose.connect(uri)
-            await service.getById(req.params.id)
-            .then((result)=>{
-                if(!result) return res.status(204).send('No match for the request')
-                return res.status(200).json(result)
-                
-            })
-            .catch((err) => {
-                console.log("Error : "+err.message)
-                return res.status(500).send('Internal server error')
-            });
-        }catch(e){
-            console.log("Error : "+e.message)
-            // await mongoose.disconnect()
-            return res.status(500).send('Internal server error')
-        }finally{
-            // await mongoose.disconnect()
-        }
-    })
-}
+  return async (req, res) => {
+    try {
+      // await mongoose.connect(uri)
+      await service
+        .getById(req.params.id)
+        .then((result) => {
+          if (!result) {
+            const responseData = {
+              status: false,
+              message: `Rendezvous with id ${req.params.id} not found`,
+              details: result,
+              http_response: {
+                message: "No match for the request",
+                code: 204,
+              },
+            };
+            return res.status(HttpStatus.OK).json(responseData);
+          }
+          const responseData = {
+            status: true,
+            message: `Rendezvous with id ${req.params.id}`,
+            details: result,
+            http_response: {
+              message: HttpStatus.getStatusText(HttpStatus.OK),
+              code: HttpStatus.OK,
+            },
+          };
+          return res.status(HttpStatus.OK).json(responseData);
+        })
+        .catch((err) => {
+          const responseData = {
+            status: false,
+            message: err,
+            details: null,
+            http_response: {
+              message: HttpStatus.getStatusText(
+                HttpStatus.INTERNAL_SERVER_ERROR
+              ),
+              code: HttpStatus.INTERNAL_SERVER_ERROR,
+            },
+          };
+          return res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(responseData);
+        });
+    } catch (e) {
+      const responseData = {
+        status: false,
+        message: e,
+        details: null,
+        http_response: {
+          message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR),
+          code: HttpStatus.INTERNAL_SERVER_ERROR,
+        },
+      };
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(responseData);
+    } finally {
+      // await mongoose.disconnect()
+    }
+  };
+};
 
 const createRendezvous = () => {
-    return (async (req,res)=>{
-        try{
-            // await mongoose.connect(uri)
-            const {id_customer, id_service, id_employe, date_heure} = req.body;
-            await service.create(id_customer, id_service, id_employe, date_heure)
-            .then((result)=>{
-                return res.status(201).json(result)
-            })
-            .catch((err) => {
-                console.log("Error : "+err.message)
-                return res.status(500).send('Internal server error')                
-            });
-        }catch(e){
-            console.log("Error : "+e.message)
-            // await mongoose.disconnect()
-            return res.status(500).send('Internal server error')
-        }finally{
-            // await mongoose.disconnect()
-        }
-    })
-}
+  return async (req, res) => {
+    try {
+      // await mongoose.connect(uri)
+      const { id_customer, id_service, id_employe, date_heure } = req.body;
+      await service
+        .create(id_customer, id_service, id_employe, date_heure)
+        .then((result) => {
+          const responseData = {
+            status: true,
+            message: "Rendezvous successfully created",
+            details: result,
+            http_response: {
+              message: HttpStatus.getStatusText(HttpStatus.OK),
+              code: HttpStatus.OK,
+            },
+          };
+          return res.status(HttpStatus.OK).json(responseData);
+        })
+        .catch((err) => {
+          const responseData = {
+            status: false,
+            message: err,
+            details: null,
+            http_response: {
+              message: HttpStatus.getStatusText(
+                HttpStatus.INTERNAL_SERVER_ERROR
+              ),
+              code: HttpStatus.INTERNAL_SERVER_ERROR,
+            },
+          };
+          return res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(responseData);
+        });
+    } catch (e) {
+      const responseData = {
+        status: false,
+        message: e,
+        details: null,
+        http_response: {
+          message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR),
+          code: HttpStatus.INTERNAL_SERVER_ERROR,
+        },
+      };
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(responseData);
+    } finally {
+      // await mongoose.disconnect()
+    }
+  };
+};
 
 const updateRendezvous = () => {
-    return (async (req,res)=>{
-        try{
-            // await mongoose.connect(uri)
-            const {id_customer, id_service, id_employe, date_heure, is_valid} = req.body;
-            await service.update(req.params.id, id_customer, id_service, id_employe, date_heure, is_valid)
-            .then((result)=>{
-                return res.status(200).json(result)
-            })
-            .catch((err) => {
-                console.log("Error : "+err.message)
-                return res.status(500).send('Internal server error')
-            });
-        }catch(e){
-            console.log("Error : "+e.message)
-            // await mongoose.disconnect()
-            return res.status(500).send('Internal server error')
-        }finally{
-            // await mongoose.disconnect()
-        }
-    })
-}
+  return async (req, res) => {
+    try {
+      // await mongoose.connect(uri)
+      const { id_customer, id_service, id_employe, date_heure, is_valid } =
+        req.body;
+      await service
+        .update(
+          req.params.id,
+          id_customer,
+          id_service,
+          id_employe,
+          date_heure,
+          is_valid
+        )
+        .then((result) => {
+          const responseData = {
+            status: true,
+            message: "Rendezvous successfully updated",
+            details: result,
+            http_response: {
+              message: HttpStatus.getStatusText(HttpStatus.OK),
+              code: HttpStatus.OK,
+            },
+          };
+          return res.status(HttpStatus.OK).json(responseData);
+        })
+        .catch((err) => {
+          const responseData = {
+            status: false,
+            message: err,
+            details: null,
+            http_response: {
+              message: HttpStatus.getStatusText(
+                HttpStatus.INTERNAL_SERVER_ERROR
+              ),
+              code: HttpStatus.INTERNAL_SERVER_ERROR,
+            },
+          };
+          return res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(responseData);
+        });
+    } catch (e) {
+      const responseData = {
+        status: false,
+        message: e,
+        details: null,
+        http_response: {
+          message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR),
+          code: HttpStatus.INTERNAL_SERVER_ERROR,
+        },
+      };
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(responseData);
+    } finally {
+      // await mongoose.disconnect()
+    }
+  };
+};
 
 const deleteRendezvous = () => {
-    return (async (req,res)=>{
-        try{
-            // await mongoose.connect(uri)
-            await service.delete_rendezvous(req.params.id)
-            .then((result)=>{
-                return res.status(204).json("Rendezvous deleted")
-            })
-            .catch((err) => {
-                console.log("Error : "+err.message)
-                return res.status(500).send('Internal server error')                
-            });
-        }catch(e){
-            console.log("Error : "+e.message)
-            // await mongoose.disconnect()
-            return res.status(500).send('Internal server error')
-        }finally{
-            // await mongoose.disconnect()
-        }
-    })
-}
+  return async (req, res) => {
+    try {
+      // await mongoose.connect(uri)
+      await service
+        .delete_rendezvous(req.params.id)
+        .then((result) => {
+          const responseData = {
+            status: true,
+            message: "Rendezvous successfully deleted",
+            details: null,
+            http_response: {
+              message: HttpStatus.getStatusText(HttpStatus.OK),
+              code: HttpStatus.OK,
+            },
+          };
+          return res.status(HttpStatus.OK).json(responseData);
+        })
+        .catch((err) => {
+          const responseData = {
+            status: false,
+            message: err,
+            details: null,
+            http_response: {
+              message: HttpStatus.getStatusText(
+                HttpStatus.INTERNAL_SERVER_ERROR
+              ),
+              code: HttpStatus.INTERNAL_SERVER_ERROR,
+            },
+          };
+          return res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json(responseData);
+        });
+    } catch (e) {
+      const responseData = {
+        status: false,
+        message: e,
+        details: null,
+        http_response: {
+          message: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR),
+          code: HttpStatus.INTERNAL_SERVER_ERROR,
+        },
+      };
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(responseData);
+    } finally {
+      // await mongoose.disconnect()
+    }
+  };
+};
 
 module.exports = {
-    getRendezvous,
-    getRendezvousById,
-    createRendezvous,
-    updateRendezvous,
-    deleteRendezvous
-}
+  getRendezvous,
+  getRendezvousById,
+  createRendezvous,
+  updateRendezvous,
+  deleteRendezvous,
+};
