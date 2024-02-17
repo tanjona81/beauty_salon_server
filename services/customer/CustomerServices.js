06bee7a4490537b85ec4e4ff43480643b6f78b14
Manisa <manisarajemisa24@gmail.com>
2024-02-18 01:15:50 +0300

    feat: add role to token

--- services/customer/CustomerServices.js
+++ services/customer/CustomerServices.js
@@ -1,77 +1,78 @@
-const Customer = require('../../schemas/CustomerSchema.js')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const Customer = require("../../schemas/CustomerSchema.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../../config/auth.config.js");
 
 const login = async (email, mdp) => {
-    const user = await Customer.findOne({email:email})
-    const test = await bcrypt.compare(mdp, user.mdp)
-    if(test) {
-        const usertoken = {
-            _id : user._id,
-            nom : user.nom
-        }
-        const accessToken = jwt.sign(usertoken,"secret")
-        return accessToken;
-    }
-    else return null;
-}
+  const user = await Customer.findOne({ email: email });
+  const test = await bcrypt.compare(mdp, user.mdp);
+  if (test) {
+    const usertoken = {
+      _id: user._id,
+      nom: user.nom,
+      role: "customer",
+    };
+    const accessToken = jwt.sign(usertoken, config.secret);
+    return accessToken;
+  } else return null;
+};
 
 const getAll = async () => {
-    return await Customer.find();
-}
+  return await Customer.find();
+};
 
 const getById = async (id) => {
-    return await Customer.findOne({_id : id});
-}
+  return await Customer.findOne({ _id: id });
+};
 
-const create = async (image, nom, prenom, tel, email, addresse, mdp) =>  {
+const create = async (image, nom, prenom, tel, email, addresse, mdp) => {
+  // Generate a salt
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  // Hash the password using the generated salt
+  const hashedmdp = await bcrypt.hash(mdp, salt);
+
+  let customer = new Customer();
+  customer.image = image;
+  customer.nom = nom;
+  customer.mdp = hashedmdp;
+  customer.prenom = prenom;
+  customer.tel = tel;
+  customer.email = email;
+  customer.addresse = addresse;
+  return await customer.save();
+};
+
+const update = async (id, image, nom, prenom, tel, email, addresse, mdp) => {
+  let customer = await Customer.findById(id);
+  if (image !== undefined) customer.image = image;
+  if (nom !== undefined) customer.nom = nom;
+  if (mdp !== undefined) {
     // Generate a salt
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
 
     // Hash the password using the generated salt
     const hashedmdp = await bcrypt.hash(mdp, salt);
-
-    let customer = new Customer();
-    customer.image = image
-    customer.nom = nom
-    customer.mdp = hashedmdp
-    customer.prenom = prenom
-    customer.tel = tel
-    customer.email = email
-    customer.addresse = addresse
-    return await customer.save()
-}
-
-const update = async (id, image, nom, prenom, tel, email, addresse, mdp) => {
-    let customer = await Customer.findById(id)
-    if(image !== undefined) customer.image = image
-    if(nom !== undefined) customer.nom = nom
-    if(mdp !== undefined) {
-        // Generate a salt
-        const saltRounds = 10;
-        const salt = await bcrypt.genSalt(saltRounds);
-
-        // Hash the password using the generated salt
-        const hashedmdp = await bcrypt.hash(mdp, salt);
-        customer.mdp = hashedmdp
-    }
-    if(prenom !== undefined) customer.prenom = prenom
-    if(tel !== undefined) customer.tel = tel
-    if(email !== undefined) customer.email = email
-    if(addresse !== undefined) customer.addresse = addresse
-    return await customer.save()
-}
+    customer.mdp = hashedmdp;
+  }
+  if (prenom !== undefined) customer.prenom = prenom;
+  if (tel !== undefined) customer.tel = tel;
+  if (email !== undefined) customer.email = email;
+  if (addresse !== undefined) customer.addresse = addresse;
+  return await customer.save();
+};
 
 const delete_customer = async (id) => {
-    return await Customer.deleteOne({ _id : id });
-}
+  return await Customer.deleteOne({ _id: id });
+};
 
 module.exports = {
-    getAll,
-    getById,
-    create,
-    update,
-    delete_customer,
-    login
-}
+  getAll,
+  getById,
+  create,
+  update,
+  delete_customer,
+  login,
+};
--- services/manager/ManagerServices.js
+++ services/manager/ManagerServices.js
@@ -1,127 +1,128 @@
-const Manager = require('../../schemas/ManagerSchema.js')
-const Rendezvous = require('../../schemas/RendezvousSchema.js')
-const RdvTracking = require('../../schemas/RendezvoustrackingSchema.js')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const Manager = require("../../schemas/ManagerSchema.js");
+const Rendezvous = require("../../schemas/RendezvousSchema.js");
+const RdvTracking = require("../../schemas/RendezvoustrackingSchema.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../../config/auth.config.js");
 
 const login = async (nom, mdp) => {
-    const user = await Manager.findOne({nom:nom})
-    const test = await bcrypt.compare(mdp, user.mdp)
-    if(test) {
-        const usertoken = {
-            _id : user._id,
-            nom : user.nom
-        }
-        const accessToken = jwt.sign(usertoken,"secret")
-        return accessToken;
-    }
-    else return null;
-}
+  const user = await Manager.findOne({ nom: nom });
+  const test = await bcrypt.compare(mdp, user.mdp);
+  if (test) {
+    const usertoken = {
+      _id: user._id,
+      nom: user.nom,
+      role: "manager",
+    };
+    const accessToken = jwt.sign(usertoken, config.secret);
+    return accessToken;
+  } else return null;
+};
 
 const getAll = async () => {
-    return await Manager.find();
-}
+  return await Manager.find();
+};
 
 const getById = async (id) => {
-    return await Manager.findOne({_id : id});
-}
+  return await Manager.findOne({ _id: id });
+};
 
-const create = async (nom, mdp) =>  {
+const create = async (nom, mdp) => {
+  // Generate a salt
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  // Hash the password using the generated salt
+  const hashedmdp = await bcrypt.hash(mdp, salt);
+
+  let manager = new Manager();
+  manager.nom = nom;
+  manager.mdp = hashedmdp;
+  return await manager.save();
+};
+
+const update = async (id, nom, mdp) => {
+  let manager = await Manager.findById(id);
+  if (nom !== undefined) manager.nom = nom;
+  if (mdp !== undefined) {
     // Generate a salt
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
 
     // Hash the password using the generated salt
     const hashedmdp = await bcrypt.hash(mdp, salt);
-
-    let manager = new Manager();
-    manager.nom = nom
-    manager.mdp = hashedmdp
-    return await manager.save()
-}
-
-const update = async (id, nom, mdp) => {
-    let manager = await Manager.findById(id)
-    if(nom !== undefined) manager.nom=nom
-    if(mdp !== undefined) {
-        // Generate a salt
-        const saltRounds = 10;
-        const salt = await bcrypt.genSalt(saltRounds);
-
-        // Hash the password using the generated salt
-        const hashedmdp = await bcrypt.hash(mdp, salt);
-        employe.mdp = hashedmdp
-    }
-    return await manager.save()
-}
+    manager.mdp = hashedmdp;
+  }
+  return await manager.save();
+};
 
 const delete_manager = async (id) => {
-    return await Manager.deleteOne({ _id : id });
-}
+  return await Manager.deleteOne({ _id: id });
+};
 
 const getTempsMoyenTravailPourChaqueEmpoye = async () => {
-    return Rendezvous.aggregate([
-        {
-            $match:{
-                is_valid: 1
-            }
-        },
-        {
-            $lookup: {
-                from: 'services',
-                localField: 'id_service',
-                foreignField: '_id',
-                as: 'service'
-            }
-        },
-        {
-            $unwind: { path: "$service" }
-        },
-        {
-            $group:{
-                _id: "$id_employe",
-                moyenne: { $avg: "$service.duree" }
-            }
-        },
-        {
-            $project: {
-                _id: "$_id",
-                moyenne: 1
-            }
-        }
-    ])
-}
+  return Rendezvous.aggregate([
+    {
+      $match: {
+        is_valid: 1,
+      },
+    },
+    {
+      $lookup: {
+        from: "services",
+        localField: "id_service",
+        foreignField: "_id",
+        as: "service",
+      },
+    },
+    {
+      $unwind: { path: "$service" },
+    },
+    {
+      $group: {
+        _id: "$id_employe",
+        moyenne: { $avg: "$service.duree" },
+      },
+    },
+    {
+      $project: {
+        _id: "$_id",
+        moyenne: 1,
+      },
+    },
+  ]);
+};
 
 const nbrReservation_jour = async () => {
-    return await RdvTracking.aggregate([
-        {
-            $group: {
-                _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
-                count: { $sum: 1 }
-             }
-        }
-    ])
-}
+  return await RdvTracking.aggregate([
+    {
+      $group: {
+        _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+};
 
 const nbrReservation_mois = async () => {
-    return await RdvTracking.aggregate([
-        {
-            $group: {
-                _id: { year: { $year: '$date' }, month: { $month: '$date' } },
-                count: { $sum: 1 }
-             }
-        }
-    ])
-}
+  return await RdvTracking.aggregate([
+    {
+      $group: {
+        _id: { year: { $year: "$date" }, month: { $month: "$date" } },
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+};
 
 module.exports = {
-    getAll,
-    getById,
-    create,
-    update,
-    delete_manager,
-    login,
-    getTempsMoyenTravailPourChaqueEmpoye,
-    nbrReservation_jour,
-    nbrReservation_mois
-}
+  getAll,
+  getById,
+  create,
+  update,
+  delete_manager,
+  login,
+  getTempsMoyenTravailPourChaqueEmpoye,
+  nbrReservation_jour,
+  nbrReservation_mois,
+};
